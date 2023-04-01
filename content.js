8923f69c2cd3fe314228f40e7287d115af9299d6
Aquib Alam <37477291+aquib399@users.noreply.github.com>
2023-04-01 15:45:09 +0530

    bugs

--- content.js
+++ content.js
@@ -1,23 +1,19 @@
 const output = document.querySelector(".output");
 const title = document.querySelector(".title");
-while (true) {
-    const pass = prompt("Enter password");
-    if (!pass) {
-        window.location = "./";
-    }
-    fetch("/find", {
-        method: "post",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({ pass: pass })
-    }).then(res => res.json()).then((data) => {
-        if (data.status){
+async () => {
+    while (true) {
+        const pass = prompt("Enter password");
+        if (!pass) {
+            window.location = "./";
+        }
+        const res = await fetch("/find", {method: "post",headers: {"Content-Type":"application/json"},body:JSON.stringify({pass:pass})});
+        const data = await res.json();
+        if(data.status){
             alert("Wrong password");
         }else{
+            title.innerHTML = data.title;
             output.innerHTML = data.content;
-            title = data.title;
             break;
         }
-    })
+    }
 }

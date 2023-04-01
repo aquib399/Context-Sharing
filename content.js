832378c04ad0b87faba9ecf9f47aed0069207ac5
Aquib Alam <37477291+aquib399@users.noreply.github.com>
2023-04-01 16:13:14 +0530

    window location and fetch

--- content.js
+++ content.js
@@ -1,7 +1,13 @@
 const output = document.querySelector(".output");
 const title = document.querySelector(".title");
 async function fetchIt(pass) {
-    const res = await fetch("/find", { method: "post", headers: { "Content-Type": "application/json" }, body: JSON.stringify({ pass: pass }) });
+    const res = await fetch("/find", {
+        method: "post",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ pass: pass });
+    })
     const data = await res.json();
     return data;
 
@@ -12,6 +18,10 @@ async function fetchIt(pass) {
     if (res.status == 401) {
         while (true) {
             const pass = prompt("Enter password");
+            if (!pass) {
+                window.location = "./";
+                return;
+            }
             res = await fetchIt(pass);
             if (res.status == 406) {
                 alert("Wrong password");

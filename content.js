e734e70bd72c0b0aeee7e645aaad4b7aaadbea14
Aquib Alam <37477291+aquib399@users.noreply.github.com>
2023-04-01 16:37:48 +0530

    app + content

--- content.js
+++ content.js
@@ -1,33 +1,29 @@
 const output = document.querySelector(".output");
 const title = document.querySelector(".title");
-async function fetchIt(pass) {
-    const res = await fetch("/find", {
-        method: "post",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({ pass: pass })
-    })
-    res.json().then(data => { return data; })
+const type = {
+    method: "post",
+    headers: {
+        "Content-Type": "application/json"
+    }
 }
-(async () => {
-    fetchIt().then(async(res) => {
-        title.innerHTML = res.title;
-        if (res.status == 401) {
-            while (true) {
-                const pass = prompt("Enter password");
-                if (!pass) {
-                    window.location = "./";
-                    return;
-                }
-                res = await fetchIt(pass);
-                if (res.status == 406) {
-                    alert("Wrong password");
-                } else {
-                    break;
-                }
+fetch("/find", type).then(res => res.json()).then(async (d) => {
+    title.innerHTML = d.title;
+    if (d.status == 401) {
+        while (true) {
+            const pass = prompt("Enter password : ");
+            if (!pass) {
+                window.location = "./";
+            }
+            type.body = JSON.stringify({ password: pass });
+            const res = await fetch("/find", type);
+            const data = await res.json();
+            if (data.status == 406) {
+                alert("Wrong password");
+            } else {
+                output.innerHTML = data.content;
+                break;
             }
         }
-        output.innerHTML = res.content;
-    });
-})();
+    }
+    output.innerHTML = d.content;
+})

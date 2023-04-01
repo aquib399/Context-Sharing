38a5c5517d4ec8846cabd06231231f373f6435cb
Aquib Alam <37477291+aquib399@users.noreply.github.com>
2023-04-01 15:47:24 +0530

    emrror

--- content.js
+++ content.js
@@ -1,19 +1,20 @@
 const output = document.querySelector(".output");
 const title = document.querySelector(".title");
-async () => {
+(async () => {
     while (true) {
         const pass = prompt("Enter password");
         if (!pass) {
             window.location = "./";
+            return;
         }
-        const res = await fetch("/find", {method: "post",headers: {"Content-Type":"application/json"},body:JSON.stringify({pass:pass})});
+        const res = await fetch("/find", { method: "post", headers: { "Content-Type": "application/json" }, body: JSON.stringify({ pass: pass }) });
         const data = await res.json();
-        if(data.status){
+        if (data.status) {
             alert("Wrong password");
         }else{
-            title.innerHTML = data.title;
-            output.innerHTML = data.content;
             break;
         }
     }
-}
+    title.innerHTML = data.title;
+    output.innerHTML = data.content;
+})();

6f16948744628d92ddac34da7bd67f227c4cbfeb
Aquib Alam <37477291+aquib399@users.noreply.github.com>
2023-04-01 16:41:17 +0530

    testing

--- content.js
+++ content.js
@@ -6,24 +6,9 @@ const type = {
         "Content-Type": "application/json"
     }
 }
-fetch("/find", type).then(res => res.json()).then(async (d) => {
-    title.innerHTML = d.title;
-    if (d.status == 401) {
-        while (true) {
-            const pass = prompt("Enter password : ");
-            if (!pass) {
-                window.location = "./";
-            }
-            type.body = JSON.stringify({ password: pass });
-            const res = await fetch("/find", type);
-            const data = await res.json();
-            if (data.status == 406) {
-                alert("Wrong password");
-            } else {
-                output.innerHTML = data.content;
-                break;
-            }
-        }
-    }
-    output.innerHTML = d.content;
-})
+async function callMe() {
+    const res = await fetch("/find", type);
+    const data = await res.json();
+    console.log(JSON.stringify(data));
+    
+}

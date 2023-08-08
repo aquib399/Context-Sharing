56df7f184cb3f4d5e15fbd80eeae7b5d5c22ad82
Aquib Alam <aquibalam399@gmail.com>
2023-08-08 11:02:22 +0530

    env fix

--- index.js
+++ index.js
@@ -1,55 +1,53 @@
 require("dotenv").config();
-console.log(process.env.PORT)
-console.log(process.env.URL)
-// const express = require("express");
-// const app = express();
-// const { MongoClient } = require("mongodb");
-// const client = new MongoClient(process.env.URl);
-// const db = client.db("ContentSharing").collection("data");
-// let content = "";
-// client.connect();
-// app.use(express.json({ limit: "20mb" }));
-// app.use(express.static(__dirname));
+const express = require("express");
+const app = express();
+const { MongoClient } = require("mongodb");
+const client = new MongoClient(process.env.URl);
+const db = client.db("ContentSharing").collection("data");
+let content = "";
+client.connect();
+app.use(express.json({ limit: "20mb" }));
+app.use(express.static(__dirname));
 
-// app.get("/", (req, res) => {
-//   res.sendFile(__dirname + "/src/home.html");
-// });
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/src/home.html");
+});
 
-// app.post("/submit", async (req, res) => {
-//   try {
-//     let result = await db.findOne({ name: req.body.name });
-//     if (result) {
-//       res.send({ status: 302 });
-//       return;
-//     }
-//     result = await db.insertOne(req.body);
-//     res.send(result);
-//   } catch (e) {
-//     res.send({ code: 503, status: e });
-//   }
-// });
+app.post("/submit", async (req, res) => {
+  try {
+    let result = await db.findOne({ name: req.body.name });
+    if (result) {
+      res.send({ status: 302 });
+      return;
+    }
+    result = await db.insertOne(req.body);
+    res.send(result);
+  } catch (e) {
+    res.send({ code: 503, status: e });
+  }
+});
 
-// app.get("/:name", async (req, res) => {
-//   const result = await db.findOne({ name: req.params.name });
-//   if (result) {
-//     content = result;
-//     res.sendFile(__dirname + "/src/content.html");
-//     return;
-//   }
-//   res.sendFile(__dirname + "/src/404.html");
-// });
+app.get("/:name", async (req, res) => {
+  const result = await db.findOne({ name: req.params.name });
+  if (result) {
+    content = result;
+    res.sendFile(__dirname + "/src/content.html");
+    return;
+  }
+  res.sendFile(__dirname + "/src/404.html");
+});
 
-// app
-//   .route("/find")
-//   .post((req, res) => {
-//     res.send(content);
-//   })
-//   .put(async (req, res) => {
-//     const result = await db.findOne({ name: req.body.name });
-//     if (result) {
-//       res.send({ status: 302 });
-//       return;
-//     }
-//     res.send({ status: 404 });
-//   });
-// app.listen(process.env.PORT);
+app
+  .route("/find")
+  .post((req, res) => {
+    res.send(content);
+  })
+  .put(async (req, res) => {
+    const result = await db.findOne({ name: req.body.name });
+    if (result) {
+      res.send({ status: 302 });
+      return;
+    }
+    res.send({ status: 404 });
+  });
+app.listen(process.env.PORT);
